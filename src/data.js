9ff9c9d486630e143e2856a723f4e08c6f000484
sfu <sfu.concepts@gmail.com>
2016-10-03 15:44:18 -0400

    data update

---
+++ src/data.js
@@ -0,0 +1,231 @@
+var data = {
+	about: {
+		company_name: 'Reactive Music',
+		company_slogan: 'Brought to you by Coalition for Queens',
+		company_address: '31-00 47th Avenue #1105 | Long Island City, NY 11101',
+		company_email: 'reactiveMusic@c4q.nyc'
+	},
+	featuredTen: {
+		drake: {albumTitle: 'Views', artist: 'Drake', albumPrice: '$13.99', genre: 'Rap & Hip-Hop', 
+			cover: "https://i.scdn.co/image/90f080afbec29a0c58509875a6dd59b3c380e353", 
+			previewSong:,
+			songs: 
+				[{songTitle: 'Keep the Family Close', price:'$1.29'},
+				{songTitle: '9', price:'$1.29'},
+				{songTitle: 'U With Me?', price:'$1.29'},
+				{songTitle: 'Feel No Ways', price:'$1.29'},
+				{songTitle: 'Hype', price:'$1.29'},
+				{songTitle: 'Weston Road Flows', price:'$1.29'},
+				{songTitle: 'Redemption', price:'$1.29'},
+				{songTitle: 'With You (feat. PARTYNEXTDOOR)', price:'$1.29'},
+				{songTitle: 'Faithful (feat. Pimp C & dvsn)', price:'$1.29'},
+				{songTitle: 'Still Here', price:'$1.29'},
+				{songTitle: 'Controlla', price:'$1.29'},
+				{songTitle: 'One Dance (feat. Wizkid & Kyla)', price:'$1.29'},
+				{songTitle: 'Grammys (feat. Future)', price:'$1.29'},
+				{songTitle: 'Childs Play', price:'$1.29'},
+				{songTitle: 'Pop Style', price:'$1.29'},
+				{songTitle: 'Too Good (feat. Rihanna)', price:'$1.29'},
+				{songTitle: 'Summers Over Interlude', price:'$1.29'},
+				{songTitle: 'Fire & Desire', price:'$1.29'},
+				{songTitle: 'Views', price:'$1.29'},
+				{songTitle: 'Hotline Bling', price:'$1.29'}]
+		},
+		solange: {albumTitle: 'A Seat at the Table', artist: 'Solange', albumPrice: '$10.99', genre: "R&B", 
+			cover:"https://i.scdn.co/image/5e2cc94e5f5229acef8b49a7895da5030a94e849",
+			previewSong:,
+			songs:
+				[{songTitle: 'Rise', price:'$1.29'},
+				{songTitle: 'Weary', price:'$1.29'},
+				{songTitle: 'Interlude: The Glory Is In You', price:'$0.69'},
+				{songTitle: 'Cranes in the Sky', price:'$1.29'},
+				{songTitle: 'Interlude: Dad Was Mad', price:'$0.69'},
+				{songTitle: 'Mad (feat. Lil Wayne)', price:'$1.29'},
+				{songTitle: "Don't You Wait", price:'$1.29'},
+				{songTitle: 'Interlude: Tina Taught Me', price:'$0.69'},
+				{songTitle: "Don't Touch My Hair (feat. Sampha)", price:'$1.29'},
+				{songTitle: 'Interlude: This Moment', price:'$0.69'},
+				{songTitle: 'Where Do We Go', price:'$1.29'},
+				{songTitle: 'Interlude: For Us By Us', price:'$1.29'},
+				{songTitle: 'F.U.B.U. (feat. The-Dream & BJ the Chicago Kid', price:'$1.29'},
+				{songTitle: 'Borderline (An Olde to Self Care) [feat. Q-Tip]', price:'$1.29'},
+				{songTitle: 'Interlude: I Got So Much Magic, You Can Have It (feat. Kelly Rowland & Nia Andrews', price:'$0.69'},
+				{songTitle: 'Junie', price:'$1.29'},
+				{songTitle: 'Interlude: No Limits', price:'$0.69'},
+				{songTitle: "Don't Wish Me Well", price:'$1.29'},
+				{songTitle: 'Interlude: Pedestals', price:'$0.69'},
+				{songTitle: 'Scales (feat. Kelela)', price:'$1.29'},
+				{songTitle: 'Closing: The Chosen Ones', price: '$0.69'}]
+		},
+		bonIver: {albumTitle: '22, A Million', artist: 'Bon Iver', albumPrice: '$9.99', genre: 'Alternative', 
+			cover:"https://i.scdn.co/image/52abd9c07908386d56681b87a3c22b54bc904ed6",
+			previewSong:,
+			songs:
+				[{songTitle: '22 (OVER SooN)', price:'$1.29'},
+				{songTitle: '10 d E A T h b R E a s T', price:'$1.29'},
+				{songTitle: '715 - CREEKS', price:'$1.29'},
+				{songTitle: '33 "GOD"', price:'$1.29'},
+				{songTitle: '29 #Strafford APTS', price:'$1.29'},
+				{songTitle: '666', price:'$1.29'},
+				{songTitle: "21 MOON WATER", price:'$1.29'},
+				{songTitle: '8 (circle)', price:'$1.29'},
+				{songTitle: "___45___", price:'$1.29'},
+				{songTitle: '00000 Million', price:'$1.29'}]
+		},
+		frankOcean: {albumTitle: 'Blonde', artist: 'Frank Ocean', albumPrice: '$9.99', genre: "Pop", 
+			cover:"https://i.scdn.co/image/14e795dfa0e03a59caa2411595175f09caaf19b8",
+			previewSong:,			
+			songs:
+				[{songTitle: 'Nikes', price:'$1.29'},
+				{songTitle: 'Ivy', price:'$1.29'},
+				{songTitle: 'Pink + White', price:'$1.29'},
+				{songTitle: 'Be Yourself', price:'$1.29'},
+				{songTitle: 'Solo', price:'$1.29'},
+				{songTitle: 'Skyline To', price:'$1.29'},
+				{songTitle: 'Self Control', price:'$1.29'},
+				{songTitle: 'Good Guy', price:'$1.29'},
+				{songTitle: 'Nights', price:'$1.29'},
+				{songTitle: 'Solo (Reprise)', price:'$1.29'},
+				{songTitle: 'Pretty Sweet', price:'$1.29'},
+				{songTitle: 'Facebook Story', price:'$1.29'},
+				{songTitle: 'Close to You', price:'$1.29'},
+				{songTitle: 'White Ferrari', price:'$1.29'},
+				{songTitle: 'Seigfried', price:'$1.29'},
+				{songTitle: 'Godspeed', price:'$1.29'},
+				{songTitle: 'Futura Free', price:'$1.29'}]
+		},
+		floridaGeorgiaLine: {albumTitle: 'Dig Your Roots', artist: 'Florida Georgia Line', albumPrice: '$12.99', genre: "Country",cover:"https://i.scdn.co/image/a29b7f8b600728f13e55d07ee94848c6daae7d91",
+			previewSong:,
+			songs:
+				[{songTitle: 'Smooth', price:'$1.29'},
+				{songTitle: 'Dig Your Roots', price:'$1.29'},
+				{songTitle: 'Life Is a Honeymoon (feat. Ziggy Marley)', price:'$1.29'},
+				{songTitle: 'H.O.L.Y.', price:'$1.29'},
+				{songTitle: 'Island', price:'$1.29'},
+				{songTitle: 'May We All (feat. Tim McGraw)', price:'$1.29'},
+				{songTitle: 'Summerland', price:'$1.29'},
+				{songTitle: 'Lifer', price:'$1.29'},
+				{songTitle: 'Good Girl, Bad Boy', price:'$1.29'},
+				{songTitle: 'Wish You Were on It', price:'$1.29'},
+				{songTitle: 'God, Your Mama, And Me (feat. Backstreet Boys)', price:'$1.29'},
+				{songTitle: 'Music Is Healing', price:'$1.29'},
+				{songTitle: "While He's Still Around", price:'$1.29'},
+				{songTitle: 'Grow Old', price:'$1.29'},
+				{songTitle: 'Heatwave', price:'$1.29'}]
+		},
+		arianaGrande: {albumTitle: 'Dangerous Woman', artist: 'Ariana Grande', albumPrice: '$6.99', genre: "Pop", 
+			cover:"https://i.scdn.co/image/234d8916b078991ccd6ebe7e005451acf3e0c22e",
+			previewSong:,
+			songs:
+				[{songTitle: 'Moonlight', price:'$1.29'},
+				{songTitle: 'Dangerous Woman', price:'$1.29'},
+				{songTitle: 'Be Alright', price:'$1.29'},
+				{songTitle: 'Into You', price:'$1.29'},
+				{songTitle: 'Side To Side (feat. Nicki Minaj)', price:'$1.29'},
+				{songTitle: 'Let Me Love You (feat. Lil Wayne)', price:'$1.29'},
+				{songTitle: 'Greedy', price:'$1.29'},
+				{songTitle: 'Leave Me Lonely (feat. Macy Gray)', price:'$1.29'},
+				{songTitle: 'Everyday (feat. Future)', price:'$1.29'},
+				{songTitle: 'Sometimes', price:'$1.29'},
+				{songTitle: "I Don't Care", price:'$1.29'},
+				{songTitle: 'Bad Decisions', price:'$1.29'},
+				{songTitle: 'Touch It', price:'$1.29'},
+				{songTitle: 'Knew Better / Forever Boy', price:'$1.29'},
+				{songTitle: 'Thinking Bout You', price:'$1.29'}]
+		},
+		vanMorrison: {albumTitle: 'Keep Me Singing', artist: 'Van Morrison', albumPrice: '$9.99', genre: 'Rock', 
+			cover: "https://images-na.ssl-images-amazon.com/images/I/61qmrGg-X5L._SY300_.jpg",
+			previewSong:,
+			songs:
+				[{songTitle: 'Let It Rhyme', price:'$1.29'},
+				{songTitle: 'Every Time I See a River', price:'$1.29'},
+				{songTitle: 'Keep Me Singing', price:'$1.29'},
+				{songTitle: 'Out In the Cold Again', price:'$1.29'},
+				{songTitle: 'Memory Lane', price:'$1.29'},
+				{songTitle: 'The Pen Is Mightier Than The Sword', price:'$1.29'},
+				{songTitle: 'Holy Guardian Angel', price:'$1.29'},
+				{songTitle: 'Share Your Love With Me', price:'$1.29'},
+				{songTitle: 'In Tiburon', price:'$1.29'},
+				{songTitle: 'Look Behind the Hill', price:'$1.29'},
+				{songTitle: 'Going Down to Bangor', price:'$1.29'},
+				{songTitle: 'Too Late', price:'$1.29'},
+				{songTitle: 'Caledonia Swing', price:'$1.29'}]
+		},
+		panicAtTheDisco: {albumTitle: 'Death of A Bachelor', artist: 'Panic! At the Disco', albumPrice: '$6.99', genre: "Alternative", 
+			cover:"https://i.scdn.co/image/2071aea7efa2dfcd43268212cc5b001cb1c3858e",
+			previewSong:,
+			songs:
+				[{songTitle: 'Victorious', price:'$1.29'},
+				{songTitle: "Don't Threaten Me with a Good Time", price:'$1.29'},
+				{songTitle: 'Hallelujah', price:'$1.29'},
+				{songTitle: "Emperor's New Clothes", price:'$1.29'},
+				{songTitle: 'Death of a Bachelor', price:'$1.29'},
+				{songTitle: 'Crazy=Genius', price:'$1.29'},
+				{songTitle: 'LA Devotee', price:'$1.29'},
+				{songTitle: 'Golden Days', price:'$1.29'},
+				{songTitle: 'The Good, the Bad and the Dirty', price:'$1.29'},
+				{songTitle: 'House of Memories', price:'$1.29'},
+				{songTitle: 'Impossible Year', price:'$1.29'}]
+		},
+		banks: {albumTitle: 'The Altar', artist: 'Banks', albumPrice: '$9.99', genre: "Alternative", 
+			cover:"https://i.scdn.co/image/b4567c14b61beec12ba4179750e9374ef4f72d67",
+			previewSong:,
+			songs:
+				[{songTitle: 'Gemini Feed', price:'$1.29'},
+				{songTitle: 'Fuck with Myself', price:'$1.29'},
+				{songTitle: 'Lovesick', price:'$1.29'},
+				{songTitle: 'Mind Games', price:'$1.29'},
+				{songTitle: 'Trainwreck', price:'$1.29'},
+				{songTitle: 'This Is Not About Us', price:'$1.29'},
+				{songTitle: 'Weaker Girl', price:'$1.29'},
+				{songTitle: 'Mother Earth', price:'$1.29'},
+				{songTitle: 'Judas', price:'$1.29'},
+				{songTitle: 'Haunt', price:'$1.29'},
+				{songTitle: 'Poltergeist', price:'$1.29'},
+				{songTitle: 'To the Hilt', price:'$1.29'},
+				{songTitle: '27 Hours', price:'$1.29'}]
+		},
+		johnMelissa: {albumTitle: 'Beautiful Surrender', artist: 'Jonathan David & Melissa Helser', albumPrice: '$10.99', genre: 'Christian & Gospel',
+			cover:"https://i.scdn.co/image/77b5058112d5d4b73967e29e67d02e82e78bcf90",
+			previewSong:,
+			songs:
+				[{songTitle: 'Beautiful Surrender', price:'$1.29'},
+				{songTitle: 'You Came (Lazarus)', price:'$1.29'},
+				{songTitle: 'Find Me', price:'$1.29'},
+				{songTitle: 'Outrageous Love', price:'$1.29'},
+				{songTitle: 'Strength', price:'$1.29'},
+				{songTitle: 'The Gospel', price:'$1.29'},
+				{songTitle: 'So Much Grace', price:'$1.29'},
+				{songTitle: 'Beautiful Jesus', price:'$1.29'},
+				{songTitle: 'First Love', price:'$1.29'},
+				{songTitle: 'Abba (Arms of a Father)', price:'$1.29'},
+				{songTitle: 'Catch the Wind', price:'$1.29'},
+				{songTitle: 'No Longer Slaves (Radio Remix) [Bonus Track]', price:'$1.29'}]
+		},
+	}
+};
+
+data.getFeatured = function() {
+  return this.featuredTen;
+};
+
+data.getAbout = function() {
+  return this.about;
+};
+
+export default data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
