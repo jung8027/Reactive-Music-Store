ea494fde4b66467b63a5d2bb40dd19b8965f1d1d
Ruben Gil <gil.r419@gmail.com>
2016-10-03 20:54:10 -0400

    initial App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import ReactDOM from 'react-dom';
 import {Router, Route, Link, hashHistory} from 'react-router';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Footer from './Footer.jsx';
+import data from './data.js';
+
+
 
 var App = React.createClass({
+  getState: function() {
+    return {cart:{numOfItems: 0, items: []}}
+  },
   render: function() {
     return (
       <div>
+        <div className="jumbotron">
+          <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <a className="navbar-brand" href="#"></a>
+              </div>
+              <ul className="nav navbar-nav">
+                <li className="active"><Link to='#'>Home</Link></li>
+                <li><Link to='/'>New Releases</Link></li>
+                <li><Link to='#'>Popular Songs</Link></li>
+                <li><Link to='#'>Deals</Link></li>
+                <li><Link to='#'>Popular Albums</Link></li>
+                <li><Link to='#'>Staff Picks</Link></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+
         <h1>Hello World!</h1>
+        <Footer data={data.about}/>
       </div>
     )
   }
