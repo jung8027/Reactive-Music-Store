74f48043cee6f27771cb38c3bd92a0b1492b6b27
Ruben Gil <gil.r419@gmail.com>
2016-10-04 16:08:49 -0400

    added and incorporated Categories.jsx

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import data from './data.js';
 import NewReleases from './NewReleases.jsx';
 import Deals from './Deals.jsx';
+import Categories from './Categories.jsx'
 import Footer from './Footer.jsx';
 // import Coverflow from 'react-coverflow';
 // import AlbumTile from './AlbumTile'
@@ -24,25 +25,34 @@ var App = React.createClass({
         <div className="banner">
           <img id="banner-img" src={require('./../images/ReactiveLogo.jpg')} />
         </div>
-          <nav className="navbar navbar-inverse">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <a className="navbar-brand" href="#"></a>
-              </div>
-              <ul className="nav navbar-nav">
-                <li className="active"><Link to='/'>New Releases</Link></li>
-                <li><Link to='deals'>Deals</Link></li>
-                <li><Link to='#'>Popular Songs</Link></li>
-                <li><Link to='#'>Popular Albums</Link></li>
-                <li><Link to='#'>Staff Picks</Link></li>
-              </ul>
+
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#"></a>
             </div>
-          </nav>
+            <ul className="nav navbar-nav">
+              <li className="active"><Link to='/'>New Releases</Link></li>
+              <li><Link to='deals'>Deals</Link></li>
+              <li><Link to='#'>Popular Songs</Link></li>
+              <li><Link to='#'>Popular Albums</Link></li>
+              <li><Link to='#'>Staff Picks</Link></li>
+            </ul>
+          </div>
+        </nav>
 
-
-        <div id="page">
-          {this.props.children}
-          <Footer data={data.about}/>
+        <div className="container sidebar-divs ">
+          <div className="row sidebar-divs ">
+            <div className="col-sm-3 sidebar-divs ">
+              <Categories />
+            </div>
+            <div className="col-sm-9">
+              <div id="page">
+                {this.props.children}
+                <Footer data={data.about}/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
