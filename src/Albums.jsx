99aa557b85c4b9430e98e1f293d658c4fe78784c
Ruben Gil <gil.r419@gmail.com>
2016-10-05 11:04:13 -0400

    edited the ternary statement in src/Albums.jsx

--- src/Albums.jsx
+++ src/Albums.jsx
@@ -9,14 +9,14 @@ var Albums = React.createClass({
     songPrice: React.PropTypes.string,
 	},
   render: function() {
-    var className = (className === undefined) ? 'albums' : className + ' albums';
+    var className = (this.props.className) ? this.props.className + ' albums' : 'albums';
     var details = [];
 
     if(this.props.songAlbum) details.push(<p>{this.props.songAlbum}</p>);
     if(this.props.songArtist) details.push(<p>{this.props.songArtist}</p>);
     if(this.props.songPrice) details.push(<p>{this.props.songPrice}</p>)
     if(this.props.faveTrack) details.push(<p>Fave Track: {this.props.faveTrack}</p>);
-    
+
     return (
       <div className={className}>
         <img className="album" src={this.props.songImg}></img>
