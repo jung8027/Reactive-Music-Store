d8f4826665c8dc0fc6dba741c69134846640e0b9
Ruben Gil <gil.r419@gmail.com>
2016-10-04 20:50:23 -0400

    modified the banner image, App.css, App.js, and added some popular albums to data.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ var App = React.createClass({
     return (
       <div>
         <div className="banner">
-          <img id="banner-img" src={require('./../images/ReactiveLogo.jpg')} />
+          <img id="banner-img" src={require('./../images/reactiveLogo.png')} />
         </div>
 
         <nav className="navbar navbar-inverse">
@@ -29,11 +29,12 @@ var App = React.createClass({
               <a className="navbar-brand" href="#"></a>
             </div>
             <ul className="nav navbar-nav">
-              <li className="active"><Link to='/'>New Releases</Link></li>
+              <li><Link to='/'>New Releases</Link></li>
               <li><Link to='deals'>Deals</Link></li>
               <li><Link to='#'>Popular Songs</Link></li>
               <li><Link to='#'>Popular Albums</Link></li>
               <li><Link to='#'>Staff Picks</Link></li>
+              <li></li>
             </ul>
           </div>
         </nav>
